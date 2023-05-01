b53a56b5aa0a44c1abfc21d8a7aaca3149c1c074
Yuma Satake <satous.y@icloud.com>
2023-05-01 22:58:30 +0900

    数あて

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");          // 数を当ててごらん
+
+    println!("Please input your guess.");   // ほら、予想を入力してね
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");     // 行の読み込みに失敗しました
+
+    println!("You guessed: {}", guess);     // 次のように予想しました: {}
 }
